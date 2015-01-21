fd92ad86ef5ae5b43d0827d4629f4f8cb2b21fa2
gwrong <gwrong@gatech.edu>
2015-01-20 19:57:05 -0500

    Initial commit

---
+++ CommentKarma.py
@@ -0,0 +1,31 @@
+import operator
+import praw
+
+# Enter account details here
+username = ''
+password = ''
+# Enter the name of the user to get
+# submission karma for
+user = ''
+
+r = praw.Reddit(user_agent='u\Testing 1.0')
+r.login(username, password)
+user_name = user
+user = r.get_redditor(user_name)
+thing_limit = None
+gen = user.get_comments(limit=thing_limit)
+karma_by_subreddit = {}
+for thing in gen:
+    subreddit = thing.subreddit.display_name
+    karma_by_subreddit[subreddit] = (karma_by_subreddit.get(subreddit, 0) + thing.score)
+#toPrint = karma_by_subreddit.items()
+toPrint = sorted(karma_by_subreddit.items(), key=operator.itemgetter(1), reverse=True)
+
+karma = 0
+for item in karma_by_subreddit.values():
+    karma = karma + item
+f = open('CommentKarma.txt','w')
+f.write(str(toPrint)) # python will convert \n to os.linesep
+f.write("\nTotal: " + str(karma))
+f.close() 
+#print (str(toPrint))
